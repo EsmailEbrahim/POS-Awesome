313b62fac272b944c16528c29f103f88b55f2a5f
Defendicon <robin001100110@gmail.com>
2026-01-17 14:23:49 +0500

    refactor: useOnlineStatus.js  to include getCurrentInstance

--- frontend/src/posapp/composables/useOnlineStatus.js
+++ frontend/src/posapp/composables/useOnlineStatus.js
@@ -1,4 +1,4 @@
-import { ref, onUnmounted } from "vue";
+import { ref, onUnmounted, getCurrentInstance } from "vue";
 
 // Singleton state
 const isOnline = ref(navigator.onLine);
@@ -9,19 +9,21 @@ const updateOnlineStatus = () => {
 };
 
 export function useOnlineStatus() {
-    if (listenersCount === 0) {
-        window.addEventListener("online", updateOnlineStatus);
-        window.addEventListener("offline", updateOnlineStatus);
-    }
-    listenersCount++;
-
-    onUnmounted(() => {
-        listenersCount--;
+    if (getCurrentInstance()) {
         if (listenersCount === 0) {
-            window.removeEventListener("online", updateOnlineStatus);
-            window.removeEventListener("offline", updateOnlineStatus);
+            window.addEventListener("online", updateOnlineStatus);
+            window.addEventListener("offline", updateOnlineStatus);
         }
-    });
+        listenersCount++;
+
+        onUnmounted(() => {
+            listenersCount--;
+            if (listenersCount === 0) {
+                window.removeEventListener("online", updateOnlineStatus);
+                window.removeEventListener("offline", updateOnlineStatus);
+            }
+        });
+    }
 
     return {
         isOnline,
