0b824198040587492ff6908c43a5f5cacce92848
Defendicon <robin001100110@gmail.com>
2026-03-11 12:00:57 +0500

    fix: correct tooltip contrast and improve cart empty state

--- frontend/src/posapp/components/pos/customer/Customer.vue
+++ frontend/src/posapp/components/pos/customer/Customer.vue
@@ -30,7 +30,7 @@
 			>
 				<!-- Edit icon (left) -->
 				<template #prepend-inner>
-					<v-tooltip text="Edit customer">
+					<v-tooltip :text="__('Edit customer')" content-class="posa-theme-tooltip">
 						<template #activator="{ props }">
 							<v-icon
 								v-bind="props"
@@ -42,7 +42,7 @@
 							</v-icon>
 						</template>
 					</v-tooltip>
-					<v-tooltip text="Reload Customers">
+					<v-tooltip :text="__('Reload customers')" content-class="posa-theme-tooltip">
 						<template #activator="{ props }">
 							<v-icon
 								v-bind="props"
@@ -62,7 +62,7 @@
 					<span v-if="isCustomerSearchLocked" class="customer-load-percent">
 						{{ customerLoadPercent }}%
 					</span>
-					<v-tooltip text="Add new customer">
+					<v-tooltip :text="__('Add new customer')" content-class="posa-theme-tooltip">
 						<template #activator="{ props }">
 							<v-icon
 								v-bind="props"
